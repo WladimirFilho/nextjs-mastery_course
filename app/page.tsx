de4abadb724c7a8bfceb11708ce4aa317c189979
wladimirfilho <wladimir.wf@gmail.com>
2024-11-27 15:19:13 +1100

    Tailwind Css Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 const Home = () => {
-  return (
-    <h1 className=" text-3xl font-bold text-white">Wellcome to new app</h1>
-  );
+  return <h1 className="subtle-regular">Wellcome to new app</h1>;
 };
 export default Home;
