251ec61013474b2dbabb4685753d39742f68903f
wladimirfilho <wladimir.wf@gmail.com>
2024-11-29 21:54:54 +1100

    Setup Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 const Home = () => {
-  return <h1 className="subtle-regular">Wellcome to new app</h1>;
+  return (
+    <>
+      <h1 className=" h1-bold ">Welcome to new app</h1>
+      <h1 className=" h1-bold font-spaceGrotesk">Welcome to new app</h1>
+    </>
+  );
 };
 export default Home;
