b9c550c2b01e08c9f0688be29a13fb56993e3f28
wladimirfilho <wladimir.wf@gmail.com>
2024-11-29 22:20:45 +1100

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ const Home = () => {
   return (
     <>
       <h1 className=" h1-bold ">Welcome to new app</h1>
-      <h1 className=" h1-bold font-spaceGrotesk">Welcome to new app</h1>
     </>
   );
 };
