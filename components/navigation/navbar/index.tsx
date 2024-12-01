d06b8ca795fa7dcff6e89bb982a2f46bc63a5b3f
wladimirfilho <wladimir.wf@gmail.com>
2024-12-02 00:39:52 +1100

    Dark and Light Theme Set up

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import Theme from "./Theme";
+
 const Navbar = () => {
   return (
-    <div className="flex-between background-light900_dark200 fixed z-50 w-full p-6 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 dark:shadow-none sm:px-12">
       <Link href="/" className=" flex items-center gap-1">
         <Image src="/images/site-logo.svg" alt="" width={23} height={23} />
         <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
@@ -12,8 +14,10 @@ const Navbar = () => {
         </p>
       </Link>
       <p>Global Search</p>
-      <div className="flex-between gap-5">Theme</div>
-    </div>
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
+    </nav>
   );
 };
 
